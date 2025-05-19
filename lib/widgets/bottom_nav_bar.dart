fa853e79487e00ea802b7c1daea697407feef527
dan <daniellahan@live.com>
2025-05-19 22:45:43 +0800

    created login screen

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+
+  const BottomNavBar({
+    Key? key,
+    required this.currentIndex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: const Color(0xFFFF8C00),
+      unselectedItemColor: Colors.grey,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list),
+          label: 'Listing',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.directions_car),
+          label: 'Ride',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: 'Chats',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.article),
+          label: 'Activity',
+        ),
+      ],
+      onTap: (index) {
+        // Handle navigation
+      },
+    );
+  }
+}
